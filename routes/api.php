789254a9f5cdc6a8cc5cdf1aa8a84c0e57e4e149
localgit <localgit@gmail.com>
2024-08-20 00:53:33 +0500

    commit a

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LeadController;
 
 
-Route::post('/leads', [LeadController::class, 'add_lead_by_api']);
+Route::post('/leads', [LeadController::class, 'add_lead_by_api']); 
 Route::get('/get_lead', [LeadController::class, 'get_lead']);
 
 // Route::middleware('auth:api')->group(function(){
