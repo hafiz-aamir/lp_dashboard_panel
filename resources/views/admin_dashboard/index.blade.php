e8c1e74b02406e98eaece0010b3e7ff0c637e76d
localgit <localgit@gmail.com>
2024-08-27 00:44:55 +0500

    change icon name

--- resources/views/admin_dashboard/index.blade.php
+++ resources/views/admin_dashboard/index.blade.php
@@ -115,7 +115,7 @@ chart.render();
                     <!-- <span class="fs-16px fw-semibold mt-2"><span style="color: #00B69B;">12</span> New Leads
                         Today.</span> -->
                 </div>
-                <img class="icon-img-main-page" src="{{ asset('assets/images/icon-1.png') }}" alt="">
+                <img class="icon-img-main-page" src="{{ asset('assets/images/Icon-1.png') }}" alt="">
             </div>
         </div>
         <div class="col-lg-3 mt-md-0 mt-3 col-md-6">
@@ -126,7 +126,7 @@ chart.render();
                     <!-- <span class="fs-16px fw-semibold mt-2"><span style="color: #00B69B;">6</span> New Leads Assigned to
                         you.</span> -->
                 </div>
-                <img class="icon-img-main-page" src="{{ asset('assets/images/icon-2.png') }}" alt="">
+                <img class="icon-img-main-page" src="{{ asset('assets/images/Icon-2.png') }}" alt="">
             </div>
         </div>
         <div class="col-lg-3 mt-lg-0 mt-3 col-md-6">
@@ -137,7 +137,7 @@ chart.render();
                     <!-- <span class="fs-16px fw-semibold mt-2"><span style="color: #00B69B;">5</span> More Leads
                         Completed.</span> -->
                 </div>
-                <img class="icon-img-main-page" src="{{ asset('assets/images/icon-3.png') }}" alt="">
+                <img class="icon-img-main-page" src="{{ asset('assets/images/Icon-3.png') }}" alt="">
             </div>
         </div>
         <div class="col-lg-3 mt-lg-0 mt-3 col-md-6">
@@ -148,7 +148,7 @@ chart.render();
                     <!-- <span class="fs-16px fw-semibold mt-2"><span style="color: #00B69B;">5</span> More Leads Started by
                         you.</span> -->
                 </div>
-                <img class="icon-img-main-page" src="{{ asset('assets/images/icon-4.png') }}" alt="">
+                <img class="icon-img-main-page" src="{{ asset('assets/images/Icon-4.png') }}" alt="">
             </div>
         </div>
     </div>  
